e16a61fd63156936d6d8c25ac113b53a58f0b9bf
miccu64 <98konrad98@gmail.com>
2025-11-08 10:17:59 +0100

    ParallelFor partial impl

--- ConcurrentWordAnalyzer/ConcurrentWordAnalyzer/WordsProcessor.cs
+++ ConcurrentWordAnalyzer/ConcurrentWordAnalyzer/WordsProcessor.cs
@@ -1,10 +1,35 @@
+using System.Collections.Concurrent;
+
 namespace ConcurrentWordAnalyzer;
 
 public class WordsProcessor
 {
-    public void SingleThread(IEnumerable<string> words)
+    public void SingleThread(ICollection<string> words)
     {
         SingleThreadWordsProcessor processor = new();
         processor.Process(words);
+    }
+
+    public void ParallelFor(ICollection<string> words)
+    {
+        Parallel.ForEach(
+            Partitioner.Create(0, words.Count),
+            () => new SingleThreadWordsProcessor(),
+            (range, _,processor) =>
+            {
+                List<string> wordsFromRange = words
+                    .Skip(range.Item1)
+                    .Take(range.Item2)
+                    .ToList();
+
+                processor.Process(wordsFromRange);
+                
+                return processor;
+            },
+            localData =>
+            {
+                
+            }
+        );
     }
 }
